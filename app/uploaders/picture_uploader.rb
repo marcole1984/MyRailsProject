a1da72c49aa06b605c2cd1384376a1e671465d3d
Hung Le <marcole1984@gmail.com>
2016-10-05 00:46:43 +0000

    add gemfile fog

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -8,7 +8,10 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
