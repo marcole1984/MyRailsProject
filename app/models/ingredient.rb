3ef21a93432a21fa9a5e49db02e2445e5d36b706
Hung Le <marcole1984@gmail.com>
2016-10-06 23:21:19 +0000

    modified styles and ingridient format

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,7 @@
 class Ingredient < ActiveRecord::Base
   
-  validates :name, presence: true, length: {minimum: 2, maximum: 25}
+  validates :name, presence: true, length: {minimum: 2, maximum: 25},
+                                   uniqueness:{case_sensitive: false}
   has_many  :recipe_ingredients
   has_many  :recipes, through: :recipe_ingredients
 end
